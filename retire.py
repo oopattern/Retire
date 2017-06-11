735ad40a592caaadb33b2458bc1e49980d63cbbf
oath <oopattern@126.com>
2017-06-12 00:14:14 +0800

    flask pass json to bootstrap

--- retire.py
+++ retire.py
@@ -1,25 +1,14 @@
 # -*- coding: utf-8 -*-    
 import os
 import sys
-import time
 import json
-import socket
-from multiprocessing import Process
 from flask import Flask,request,render_template
-# use for own
-sys.path.append("base/core")
-from packet import *
-from encrypt import *
-from clienthandler import *
-from tcpclient import *
-from reactor import *
-import server_unit_test
 
 app = Flask(__name__)
 
 # 展示退休页面
 @app.route('/retire', methods=['GET'])
-def ShowRetire():
+def ShowRetire(choice=u'普通场'):
     putongLevel = [{"host": "192.168.201.75",  "level": 201, "playnum": 276, "option": "None"},
                     {"host": "192.168.201.159", "level": 202, "playnum": 389, "option": "None"}]
     laiziLevel = [{"host": "192.168.201.138", "level": 2001, "playnum": 135, "option": "None"},
@@ -29,50 +18,29 @@ def ShowRetire():
     doublelaiziLevel = [{"host": "192.168.201.162", "level": 5001, "playnum": 268, "option": "None"},
                         {"host": "192.168.201.143", "level": 5002, "playnum": 367, "option": "None"},
                         {"host": "192.168.201.41",  "level": 5003, "playnum": 198, "option": "None"}]
-    return render_template('retire.html', changci=json.dumps(fourLevel))
+    allLevel = {u'普通场': putongLevel,
+                u'癞子场': laiziLevel,
+                u'双癞子场': doublelaiziLevel,
+                u'四人场': fourLevel}
+    return render_template('retire.html', changci=json.dumps(allLevel[choice]))
     
 # 处理退休表单请求
 @app.route('/retire', methods=['POST'])
 def HandleRetire():
-    return 'ooooo request'
-
-def ProcessCommand(name):
-    while(1):
-        print('[%s] run child process %s (%s)' % (time.asctime(), name, os.getpid()))
-        sys.stdout.flush()
-        time.sleep(5)
-        
-def UnitTestPacket():
-    outPkg = OutPacket()
-    outPkg.Begin(0x1122)
-    outPkg.WriteInt32(0x1122)
-    outPkg.WriteByte(0xCC)
-    outPkg.WriteString("123")
-    outPkg.WriteBinary("1234")
-    outPkg.End()
-    print 'outPkg.len=', len(outPkg.list_buff)
-    print 'outPkg.str_buff=', ','.join(map(lambda x: hex(ord(x)), outPkg.list_buff))
-    
-def UnitTestClient():
-    parser = CPacketParser()
-    server = server_unit_test.TestServer()
-    reactor = CReactor.Instance()
-    reactor.SetPacketParser(parser)
-    
-    client = CTcpClient(reactor, server)
-    client.InitConnect("192.168.201.159", 9999)
-    reactor.RunEventLoop()
-    
-def RunWebServer(name):
-    print('run web child process pid[%d]' % os.getpid())
-    app.run(host='localhost', port=8088, debug=True)
+    allop = request.form
+    if(allop.has_key('allretire')):
+        return '<p>' + u'执行全部退休' + '</p>'
+    elif(allop.has_key('oneretire')):
+        return '<p>' + u'退休一个场次' + '</p>'
+    elif(allop.has_key('opmenu') and allop['opmenu'] != ''):
+        return ShowRetire(allop['opmenu'])
+    return 'submit option no found...'
     
 if __name__ =='__main__':
-#     UnitTestPacket()
-    webSrvP = Process(target=RunWebServer, args=('Process WebServer',))
-    webSrvP.start()
-#     UnitTestClient()
     print('say goodbye!')
+    app.run(host='localhost', port=8088, debug=True)
+    
+    
     
     
     
