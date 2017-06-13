54ae64cfde7c7e452741a8fd1d639a6e28ee302d
unknown <DiLi@boyaa.com>
2017-06-13 23:04:58 +0800

    bootstrap table提交返回单行信息

--- retire.py
+++ retire.py
@@ -9,15 +9,15 @@ app = Flask(__name__)
 # 展示退休页面
 @app.route('/retire', methods=['GET'])
 def ShowRetire(choice=u'普通场'):
-    putongLevel = [{"host": "192.168.201.75",  "level": 201, "playnum": 276, "option": "None"},
-                    {"host": "192.168.201.159", "level": 202, "playnum": 389, "option": "None"}]
-    laiziLevel = [{"host": "192.168.201.138", "level": 2001, "playnum": 135, "option": "None"},
-                  {"host": "192.168.201.142", "level": 2002, "playnum": 498, "option": "None"}]
-    fourLevel = [{"host": "192.168.201.189", "level": 10086, "playnum": 371, "option": "None"},
-                 {"host": "192.168.201.5",   "level": 20033, "playnum": 496, "option": "None"}]
-    doublelaiziLevel = [{"host": "192.168.201.162", "level": 5001, "playnum": 268, "option": "None"},
-                        {"host": "192.168.201.143", "level": 5002, "playnum": 367, "option": "None"},
-                        {"host": "192.168.201.41",  "level": 5003, "playnum": 198, "option": "None"}]
+    putongLevel = [{"host": "192.168.201.75",  "level": 201, "playnum": 276},
+                    {"host": "192.168.201.159", "level": 202, "playnum": 389}]
+    laiziLevel = [{"host": "192.168.201.138", "level": 2001, "playnum": 135},
+                  {"host": "192.168.201.142", "level": 2002, "playnum": 498}]
+    fourLevel = [{"host": "192.168.201.189", "level": 10086, "playnum": 371},
+                 {"host": "192.168.201.5",   "level": 20033, "playnum": 496}]
+    doublelaiziLevel = [{"host": "192.168.201.162", "level": 5001, "playnum": 268},
+                        {"host": "192.168.201.143", "level": 5002, "playnum": 367},
+                        {"host": "192.168.201.41",  "level": 5003, "playnum": 198}]
     allLevel = {u'普通场': putongLevel,
                 u'癞子场': laiziLevel,
                 u'双癞子场': doublelaiziLevel,
@@ -27,8 +27,22 @@ def ShowRetire(choice=u'普通场'):
 # 处理退休表单请求
 @app.route('/retire', methods=['POST'])
 def HandleRetire():
-    allop = request.form
-    return json.dumps(allop)
+    allop = request.form['opform'] # dict data type, key: opform
+    return allop
+    return 'submit option no found...'
+    
+    """
+    if(allop.has_key('opform')):
+        return json.dumps(allop['opform'])
+    return 'submit option no found...'
+ """
+ 
+    """   
+    if(allop.has_key('retire')):
+        return json.dumps(allop)
+    elif(allop.has_key('levelswitch')):
+        return ShowRetire(allop['levelswitch'])
+    """
 
     """
     if(allop.has_key('allretire')):
@@ -42,7 +56,8 @@ def HandleRetire():
     
 if __name__ =='__main__':
     print('say goodbye!')
-    app.run(host='localhost', port=8088, debug=True)
+    app.run(host='localhost', port=8088)
+#     app.run(host='localhost', port=8088, debug=True)
     
     
     
