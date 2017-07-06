67475b994218d4eff5082e6aac18eb025eb7be03
oopattern <oopattern@126.com>
2017-07-06 20:53:51 +0800

    增加导航栏实现

--- views/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from flask import Flask,request,render_template
-
-# 定义全局的app
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
-
-from views import retire, rank
